292e2a2fd9a5e74a4e96714409ed4b72647faf55
Luacrative <afif317@gmail.com>
2024-02-02 23:39:23 -0800

    Dialogue design

--- src/screens/game.js
+++ src/screens/game.js
@@ -1,34 +1,63 @@
 import config from "../scripts/config.js";
 import makeGrid from "../scripts/grid.js";
 
-const game = document.querySelector("#game");
 const GRID_SIZE = config.GRID_SIZE;
+const DIALOGUES = {
+    start: () => "Take the first shot when you're ready! We got this"
+}
 
-const makeHeader = (text, color, parent) => { 
+const game = document.querySelector("#game");
+
+const makeHeader = (text, color, parent) => {
     const header = document.createElement("h2");
     header.textContent = text;
     header.classList.add("grid-header");
     header.classList.add(color);
     parent.appendChild(header);
+
     return header;
+}
+
+const makeDialogue = parent => {
+    const dialogue = document.createElement("div");
+    dialogue.classList.add("dialogue-container");
+    parent.appendChild(dialogue);
+
+    const header = document.createElement("h2");
+    header.classList.add("dialogue-header");
+    dialogue.appendChild(header);
+
+    const msg = document.createElement("p");
+    msg.classList.add("dialogue-msg");
+    dialogue.appendChild(msg);
+
+    const setText = (title, text) => {
+        header.textContent = title;
+        msg.textContent = text;
+    }
+
+    return { dialogue, setText };
 }
 
 const start = () => {
     const gridsContainer = document.createElement("div");
-    gridsContainer.classList.add("grids"); 
+    gridsContainer.classList.add("grids");
     game.appendChild(gridsContainer);
 
-    const grid1Container = document.createElement("div"); 
-    const header1 = makeHeader("You", "blue", grid1Container);
-    header1.classList.add("turn-selected");
+    const grid1Container = document.createElement("div");
+    makeHeader("You", "blue", grid1Container).classList.add("turn-selected");
     gridsContainer.appendChild(grid1Container);
-    
+
     const grid2Container = document.createElement("div");
-    const header2 = makeHeader("Enemy", "red", grid2Container);
+    grid2Container.classList.add("reverse");
+    makeHeader("Enemy", "red", grid2Container);
     gridsContainer.appendChild(grid2Container);
 
     var [gridCells1, gridShips1] = makeGrid(GRID_SIZE, grid1Container);
     var [gridCells2, gridShips2] = makeGrid(GRID_SIZE, grid2Container, true);
+
+    const dialogue = makeDialogue(game);
+    dialogue.setText("General", DIALOGUES.start());
 
     game.classList.remove("hidden");
 };
